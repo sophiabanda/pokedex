a9993c63d619d178626fe5eef26318e0e0a1ca11
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2022-12-07 13:02:33 -0500

    experimenting with complex data types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-alert('Hello, world!');
 
 let favoriteFood = 'Mexican Food';
-document.write(favoriteFood);
+// document.write(favoriteFood);
+
+console.log(favoriteFood)
+
+
+// let self = {
+//     name: 'Sophia',
+//     age: 39,
+//      child: {
+//         name: 'Tulo',
+//         age: '10 months'
+//     }
+// }
+
+// console.log(self)
+// self.husband = 'Trey';
+// console.log(self)
+
+let myArray = [27, 9, 13]
+
+console.log(myArray[1])
+
+
