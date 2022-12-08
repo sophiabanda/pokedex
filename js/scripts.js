6b839e895bbbddf1d8c56617c5ebbcd66e10ff70
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2022-12-08 14:10:34 -0500

    added note comments describing work

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,11 @@ let pokemonList = [
 for (let i = 0; i< pokemonList.length; i ++) {
     //Using string interpolation and dot notation we have accessed the objects in the pokemon array.
     //We aree interating over the array and printing the results to the dom for each found name + weight.
+
     // console.log(`${pokemonList[i].name} weighs: ${pokemonList[i].weight}lbs. ` + "<br>")
+
+    //Here we use a conditional within out loop that will determine which pokemon is a 'strong weight' at over 25lbs
+    //once they're found, they will recieve a special message with their name and weight. 
     if (pokemonList[i].weight > 25) {
         document.write(`${pokemonList[i].name} has the most muscle mass, weighing in at ${pokemonList[i].weight}! ` + "<br>");
     } else if (pokemonList[i].weight < 25) {
