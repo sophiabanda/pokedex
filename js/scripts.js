625a8f2a7fb7c526d53b605564c2afb15321dd69
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2022-12-31 12:17:25 -0500

    added class to buttons to fix exlusion of close button in modal during search

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ let pokemonRepo = (function() {
     $(document).ready(function () {
         $('#search-form').on("keyup", function() {
             let value = $(this).val().toLowerCase();
-            $('.btn').filter(function() {
+            $('.pokebutton').filter(function() {
                 $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
             })
         })
@@ -52,7 +52,7 @@ let pokemonRepo = (function() {
 
         let pokeList = $('.list-group');
         let pokeItem = $('<li class="col"></li> ');
-        let pokeButton = $('<button class="btn btn-light" data-target="#poke-modal" data-toggle="modal">' + pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) + '</button>');
+        let pokeButton = $('<button class="btn btn-light pokebutton" data-target="#poke-modal" data-toggle="modal">' + pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) + '</button>');
         pokeItem.append(pokeButton);
         pokeList.append(pokeItem);
         pokeButton.on('click', function() {
